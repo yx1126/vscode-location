6cbfdba63a05791fd8a0b96548445f25793947dc
yx1126 <yx17714503091@163.com>
2025-05-20 15:00:08 +0800

    feat: Add setting configuration

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,18 @@
 import { commands, window, workspace } from "vscode";
 import type { ExtensionContext } from "vscode";
 
+const CONFIG_KEY = "vscode.location";
+
 export function activate(context: ExtensionContext) {
     const disposable = commands.registerCommand("reallyx.location", async () => {
         const editor = window.activeTextEditor;
         if(!editor) return;
         const wsFolder = workspace.getWorkspaceFolder(editor.document.uri);
         if(wsFolder) {
-            await commands.executeCommand("workbench.files.action.collapseExplorerFolders");
+            const isFoldOther = workspace.getConfiguration(CONFIG_KEY).get("fold.others");
+            if(isFoldOther) {
+                await commands.executeCommand("workbench.files.action.collapseExplorerFolders");
+            }
             await commands.executeCommand("workbench.files.action.showActiveFileInExplorer");
         }
     });
